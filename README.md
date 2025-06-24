9c6ab8cf4ab9a412ac1c90f15476ce8d7caf06ec
Jilano5 <jeremy.palaffre@gmail.com>
2025-06-24 19:56:03 +0200

    fix disruptions and coverage

--- README.md
+++ README.md
@@ -52,10 +52,10 @@ To use this module, add it to the modules array in the `config/config.js` file:
         departureStationUIC: "stop_area:OCE:SA:87286005",
         arrivalStationUIC: "stop_area:OCE:SA:87286583",
         apiKey: "", // You must add your API key
-	numberDays: 2,
+	    numberDays: 2,
         dateFormat: 'dddd HH:mm', // display for example with french locale Jeudi 08:43
-        displayCo2: true,
-        coverage: "fr-ne",
+        displayC02: true,
+        coverage: "sncf",
     }
 },
 ```
@@ -82,7 +82,7 @@ The following properties can be configured:
 | `displayC02` | Display the C02 emissions for your trip. <br><br> **Possible values:** `true` or `false` <br> **Default value:** `false`
 | `displayHeaders` | Display columns headers. <br><br> **Possible values:** `true` or `false` <br> **Default value:** `true`
 | `dateFormat` | Format to use for the date of train.<br> **Caution:**<br>h display hours in 12h format / H in 24h<br>for minutes, it is mm. MM display the month number<br><br> **Possible values:** [Moment.js formats](https://momentjs.com/docs/#/parsing/string-format/) <br> **Default value:** `llll`
-| `coverage` | Coverage of departure station **REQUIRED**<br>[Check your coverage](https://www.navitia.io/datasets) Select a city near your departure station and check the coverage
+| `coverage` | Coverage of departure station<br>[Check your coverage](https://www.navitia.io/datasets) Select a city near your departure station and check the coverage (From now coverage is always sncf)
 
 ## Further information and support
 
--- node_helper.js
+++ node_helper.js
@@ -20,7 +20,7 @@ module.exports = NodeHelper.create({
 		* Calls processTransports on succesfull response.
 	*/
     updateTimetable: function () {
-        var url = "https://api.navitia.io/v1/coverage/" + this.config.coverage + "/journeys?from=" + this.config.departureStationUIC + "&to=" + this.config.arrivalStationUIC + "&datetime=" + moment().toISOString() + "&count=" + this.config.numberDays + "&max_nb_transfers=" + this.config.maxNbTransfers;
+        var url = "https://api.navitia.io/v1/coverage/" + this.config.coverage + "/journeys?from=" + this.config.departureStationUIC + "&to=" + this.config.arrivalStationUIC + "&datetime=" + moment().toISOString() + "&count=" + this.config.numberDays + "&max_nb_transfers=" + this.config.maxNbTransfers + "&data_freshness=realtime";
 
         if (this.config.debugging) console.log(url);
 
@@ -53,28 +53,9 @@ module.exports = NodeHelper.create({
         });
     },
 
-    /* getDisruptionInfo()
-        * Retrieve informations from disruption and departure time
-        * argument disruption - list of all disruptions
-        * argument idSearched - Id which we want info
-    */
-    getDisruptionInfo: function (disruptions, idSearched) {
-        // Searching our disruption ID in all disruption
-        for (var i = 0; i < disruptions.length; i++) {
-            if (disruptions[i].disruption_id == idSearched) {
-                var _disruptionInfo = {};
-                // Searching our depart stop in List of impacted stops
-                var _impactedStops = disruptions[i].impacted_objects[0].impacted_stops;
-                for (var j = 0; j < _impactedStops.length; j++) {
-                    if (_impactedStops[j].stop_point.id == this.config.departureStationUIC) {
-                        _disruptionInfo['amended_departure_time'] = _impactedStops[j].amended_departure_time;
-                        _disruptionInfo['cause'] = _impactedStops[j].cause;
-
-                        return _disruptionInfo;
-                    }
-                }
-            }
-        }
+    extractUIC: function (id) {
+        const match = id.match(/SNCF:\d{8}/);
+        return match ? match[0] : null;
     },
 
     /* processTransports(data)
@@ -91,11 +72,9 @@ module.exports = NodeHelper.create({
         responseInJson = data.journeys;
 
         // we search if there are disruptions
+        var disruptions = 0;
         if (data.disruptions) {
             disruptions = data.disruptions;
-        }
-        else {
-            disruptions = 0;
         }
 
         for (var i = 0; i < responseInJson.length; i++) {
@@ -107,19 +86,21 @@ module.exports = NodeHelper.create({
                     var ride = nextTrain.sections[j];
 
                     if (ride.mode != "walking") {
-                        var date = ride.departure_date_time;
                         var originalDate = ride.base_departure_date_time;
-                        var alert = "";
+                        var date = ride.departure_date_time;
 
                         // on récupère l'id de la disruptionMessage
                         var idDisruption = 0;
 
-                        if (alert.length > 0) {
-                            if (alert[0].type == 'disruption') {
-                                idDisruption = alert[0].id;
+                        var links = ride.display_informations.links;
+                        if (links.length > 0) {
+                            for (var l = 0; l < links.length; l++) {
+                                if (links[l].type == 'disruption') {
+                                    idDisruption = links[l].id;
+                                }
                             }
-                        }
-
+                        }     
+                        
                         // on parcours les disruption jusqu'a retrouver la bonne
                         var disruptionInfo = null;
 
@@ -133,10 +114,8 @@ module.exports = NodeHelper.create({
                                     var impactedStops = disruptions[k].impacted_objects[0].impacted_stops;
 
                                     for (var l = 0; l < impactedStops.length; l++) {
-                                        if (impactedStops[l].stop_point.id == this.config.departureStationUIC) {
-                                            disruptionInfo['amended_departure_time'] = date.substring(0, 9) + impactedStops[l].amended_departure_time;
-                                            disruptionInfo['amended_departure_time'] = disruptionInfo['amended_departure_time'].substring(disruptionInfo['amended_departure_time'].lastIndexOf(" ") + 1);
-                                            disruptionInfo['amended_departure_time'] = moment(disruptionInfo['amended_departure_time']).format(this.config.dateFormat);
+                                        if (this.extractUIC(impactedStops[l].stop_point.id) && this.extractUIC(impactedStops[l].stop_point.id) === this.extractUIC(this.config.departureStationUIC)) {                                            
+                                            disruptionInfo['amended_departure_time'] = moment(ride.departure_date_time).format(this.config.dateFormat);
                                             disruptionInfo['cause'] = impactedStops[l].cause;
 
                                             if (this.config.debugging) console.log(disruptionInfo);
@@ -146,22 +125,12 @@ module.exports = NodeHelper.create({
                             }
                         }
 
-                        if (disruptionInfo !== null && disruptionInfo.hasOwnProperty("amended_departure_time")) {
-                            date = disruptionInfo.amended_departure_time;
-                            date = date.substring(date.lastIndexOf(" ") + 1);
-                        }
-                        else {
-                            date = date.substring(date.lastIndexOf(" ") + 1);
-                        }
-
                         var delay = 0;
 
-                        if (originalDate !== undefined && date !== undefined) {
+                        if (originalDate !== undefined && date !== undefined) {                 
                             if (this.config.debugging) console.log("Date : " + date);
                             if (this.config.debugging) console.log("Original date : " + originalDate);
-
-                            originalDate = originalDate.substring(originalDate.lastIndexOf(" ") + 1);
-
+                            
                             delay = moment(date).diff(moment(originalDate), "minutes");
                         }
 
@@ -172,7 +141,7 @@ module.exports = NodeHelper.create({
                             originalDate: ride.type != "waiting" && originalDate !== undefined ? moment(originalDate).format(this.config.dateFormat) : null,
                             destination: self.findDestination(ride),
                             duration: self.timeFormatting(ride.duration / 60), // duration in minutes
-                            delay: self.timeFormatting(delay),
+                            delay: delay,
                             disruptionInfo: disruptionInfo,
                             state: nextTrain.status,
                             endOfJourney: self.isEndOfTheDay(ride.to),
